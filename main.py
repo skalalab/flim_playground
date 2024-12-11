d5960d2fc75c4861e30801010ac9d7e4e66942be
Wenxuan Zhao <xiaodoubizwx@gmail.com>
2024-12-11 10:16:37 -0600

    try top menu layout

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import streamlit as st
+from navigation import render_top_menu
 
-st.set_page_config(page_title="FLIM Analysis Pipeline", layout="wide")
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
 
-# Sidebar navigation
-st.sidebar.title("Navigation")
-st.sidebar.write("Select a step to proceed.")
+# Hide the sidebar completely with CSS
+hide_sidebar_style = """
+<style>
+.css-1cpxqw2 {visibility: hidden;} /* This may need adjusting depending on Streamlit version */
+</style>
+"""
+st.markdown(hide_sidebar_style, unsafe_allow_html=True)
+
+# Render the top menu on the main page
+render_top_menu()
+
+st.title("Home")
+st.write("This is the main page. Use the top menu to navigate to other pages.")
