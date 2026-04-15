ccb6df0e1cde8f5a02ca88ae92b74a640b245ad8
Wenxuan Zhao <xiaodoubizwx@gmail.com>
2026-04-15 16:33:33 -0500

    speed up convolution

--- src/fit_helper.py
+++ src/fit_helper.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.signal import fftconvolve
 
 def upsample_irf(irf, scale=10):
     return np.interp(np.linspace(0, len(irf), len(irf)*scale), np.arange(len(irf)), irf)
@@ -24,8 +25,7 @@ def forward_pass(amp1, t1, offset, shifted_irf, time_axis, amp2=None, t2=None, a
         decay = amp1 * np.exp(-time_axis / t1) + amp2 * np.exp(-time_axis / t2) 
     else:
         decay = amp1 * np.exp(-time_axis / t1) 
-    # convolve the decay with the shifted IRF, finally add the offset
-    convolved_decay = np.convolve(decay, shifted_irf)[:len(time_axis)] + offset
+    convolved_decay = fftconvolve(decay, shifted_irf)[:len(time_axis)] + offset
     
     return convolved_decay
 def nll_poisson(fitted, data, start, end):
